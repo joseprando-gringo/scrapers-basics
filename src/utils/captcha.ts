6baf2c9eb81b01118a36d47a07fe54b7c549bc5a
Luis Felipe <luisfelipesena@gmail.com>
2023-02-24 15:35:56 -0300

    fix: getReCaptchaV2Response

--- src/utils/captcha.ts
+++ src/utils/captcha.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-export async function getReCaptchaV2Response (googleSiteKey: string, pageUrl: string): Promise<string> {
+export async function getReCaptchaV2Response (googleSiteKey: string, pageUrl: string, type = 'ReCaptchaV2', Authorization = 'Api-Key '): Promise<string> {
   return (await axios.post('https://k8s.gringo.com.vc/captcha-service-v2/v1/captcha', {
-    type: 'ReCaptchaV2',
+    type: type,
     args: {
       googleSiteKey,
       pageUrl
     }
   }, {
     headers: {
-      Authorization: 'Api-Key ' // 9810abe7-c758-4b9d-a56e-a806b43494d3
+      Authorization // 9810abe7-c758-4b9d-a56e-a806b43494d3
     }
   })).data.response;
 }

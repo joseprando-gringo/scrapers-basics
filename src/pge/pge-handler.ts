eee1e3abb8e1d79ef28e486dd625221461a52923
Jose Prando <jose@gringo.com.vc>
2023-03-14 18:53:05 -0300

    refactor

--- src/pge/pge-handler.ts
+++ src/pge/pge-handler.ts
@@ -1,37 +1,46 @@
 import axios from 'axios';
 import { RequestHandler } from 'express';
 import { Cookie } from 'tough-cookie';
-import { load } from 'cheerio';
+import { CheerioAPI, Element, load } from 'cheerio';
 import fs from 'fs';
 
 import { getReCaptchaV2Response } from '../utils/captcha';
+import { data } from 'cheerio/lib/api/attributes';
 
 const GARE_LIQUIDACAO_URL = 'https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao.jsf';
 
-export const pgeHandler: RequestHandler = async (req, res) => {
-  const { renavam } = req.body;
-
- 
-  // 1. GET gareLiquidacao
+const getFirstPageCookieAndJSessionIdAndViewState = async (): Promise<{ cookie: string, jsessionId: string, viewState: string }> => {
   console.log('1. GET GARE_LIQUIDACAO_URL');
   const firstPageResponse = await axios.get(GARE_LIQUIDACAO_URL);
 
   // 2. Capturar Cookie (Set-Cookie)
   console.log('2. Capturar Cookie (Set-Cookie)');
-  const cookiesString = (firstPageResponse.headers['set-cookie'] ?? [])
+  const cookie = (firstPageResponse.headers['set-cookie'] ?? [])
     .map(cookieStr => Cookie.parse(cookieStr))
     .map(cookie => cookie?.cookieString())
     .join('; ')
-    .trim();  
+    .trim();
   const jsessionId = (firstPageResponse.headers['set-cookie'] ?? [])
     .map(cookieStr => Cookie.parse(cookieStr))
     .map(cookie => cookie?.value)[0];
 
   console.log('3. Capturar os Input Hidden');
-  let $ = load(firstPageResponse.data);
+  const $ = load(firstPageResponse.data);
 
-  let viewState = $('#javax\\.faces\\.ViewState').val()?.toString() ?? ''
+  const viewState = $('#javax\\.faces\\.ViewState').val()?.toString()
 
+  if (!cookie || !jsessionId || !viewState) {
+    throw new Error('');
+  }
+
+  return {
+    cookie,
+    jsessionId,
+    viewState
+  }
+}
+
+const makePostRequestWithEmptyValues = async (viewState: string, jsessionId: string, cookie: string): Promise<void> => {
   console.log('4. POST Vazio');
   let formPostParams = new URLSearchParams({
     'AJAXREQUEST': '_viewRoot',
@@ -47,29 +56,30 @@ export const pgeHandler: RequestHandler = async (req, res) => {
   let formPostResponse = await axios.post(`${GARE_LIQUIDACAO_URL};jsessionid=${jsessionId}`, formPostParams, {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Cookie': cookiesString,
+      'Cookie': cookie,
     },
     maxRedirects: 0
   });
+}
 
+const makePostRequestWithCaptchaAndValues = async (renavam: string, jsessionId: string, viewState: string, cookie: string) => {
   console.log('5. Captcha');
   const captchaResponse = await getReCaptchaV2Response('6Le9EjMUAAAAAPKi-JVCzXgY_ePjRV9FFVLmWKB_', GARE_LIQUIDACAO_URL);
   
   let location: string | undefined
   console.log('6. POST Consulta');
-  formPostParams = new URLSearchParams({
-    adesaoForm: 'adesaoForm',
-    'adesaoForm:j_id70': 'RENAVAM',
-    'adesaoForm:renavam': renavam,
-    'adesaoForm:j_id76': 'Consultar',
-    'g-recaptcha-response': captchaResponse,
-    'javax.faces.ViewState': viewState
-  }).toString()
   try {
-    formPostResponse = await axios.post(`${GARE_LIQUIDACAO_URL};jsessionid=${jsessionId}`, formPostParams, {
+    await axios.post(`${GARE_LIQUIDACAO_URL};jsessionid=${jsessionId}`, new URLSearchParams({
+      adesaoForm: 'adesaoForm',
+      'adesaoForm:j_id70': 'RENAVAM',
+      'adesaoForm:renavam': renavam,
+      'adesaoForm:j_id76': 'Consultar',
+      'g-recaptcha-response': captchaResponse,
+      'javax.faces.ViewState': viewState
+    }).toString(), {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': cookiesString
+        'Cookie': cookie
       },
       maxRedirects: 0
     });
@@ -82,22 +92,144 @@ export const pgeHandler: RequestHandler = async (req, res) => {
   }
 
   if (!location) {
-    return res.status(500).send();
+    throw new Error('makePostRequestWithCaptchaAndValues: location cannot be null or empty')
   }
   
-  // console.log('location1', location);
-  const primeiroRes = await axios.get(location.replace('http', 'https'), {
+  const getLocationResponse = await axios.get(location.replace('http', 'https'), {
     headers: {
-      Cookie: cookiesString
+      Cookie: cookie
     }
   });
 
-  $ = load(primeiroRes.data);
-  viewState = $('#javax\\.faces\\.ViewState').val()?.toString() ?? ''
+  const $ = load(getLocationResponse.data);
+  const newViewState = $('#javax\\.faces\\.ViewState').val()?.toString()
 
-  // Se não encontrar gareForm\\:dataTable\\:tb, então é 1
-  const dataTablelLinks = $('#gareForm\\:dataTable\\:tb tr a').get()
-  if (!dataTablelLinks || dataTablelLinks.length === 0) {
+  if (!newViewState) {
+    throw new Error('makePostRequestWithCaptchaAndValues: viewState cannot be null or empty');
+  }
+
+  const dataTablelLinks = $('#gareForm\\:dataTable\\:tb tr a').get();
+
+  return {
+    viewState: newViewState,
+    formDataElementNames: dataTablelLinks.map(el => `gareForm:dataTable${$(el).attr('onclick')?.toString().split('gareForm:dataTable')[1].replace('\':\'', '')}`) || [],
+    isMultiple: dataTablelLinks.length > 1
+  }
+}
+
+const simulateBackButtonClick = async (viewState: string, cookie: string) => {
+  let location: string | undefined;
+  try {
+    await axios.post('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoDetalhe.jsf', new URLSearchParams({
+      gareForm: 'gareForm',
+      'gareForm:j_id80': 'Retornar',
+      'javax.faces.ViewState': viewState
+    }).toString(), {
+      headers: {
+        Cookie: cookie,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      maxRedirects: 0
+    })
+  } catch (err: any) {
+    if (err.response.status === 302) {
+      location = err.response.headers['location'];
+    } else {
+      throw err;
+    }
+  }
+
+  if (!location) {
+    throw new Error('goBack: location empty, aborting');
+  }
+
+  const getLocationResponse = await axios.get(location.replace('http', 'https'), {
+    headers: {
+      Cookie: cookie
+    }
+  });
+
+  const $ = load(getLocationResponse.data);
+  const newViewState = $('#javax\\.faces\\.ViewState').val()?.toString();
+  if (!newViewState) {
+    throw new Error('goBack: newViewState empty, aborting');
+  }
+
+  return {
+    viewState: newViewState
+  }
+}
+
+const makePostRequestToPrepareDownload = async (formDataElementName: string, viewState: string, cookie: string) => {
+  let location: string | undefined;
+  try {
+    await axios.post('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoLista.jsf', 
+    new URLSearchParams({
+      gareForm: 'gareForm',
+      'javax.faces.ViewState': viewState,
+      [formDataElementName]: formDataElementName
+    }).toString(), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Cookie: cookie
+      },
+      maxRedirects: 0
+    })
+  } catch (err: any) {
+    if (err.response.status === 302) {
+      location = err.response.headers['location'];
+    } else {
+      throw err;
+    }
+  }
+
+  if (!location) {
+    throw new Error('makePostRequestToPrepareDownload: location empty, aborting');
+  }
+
+  const getLocationResponse = await axios.get(location.replace('http', 'https'), {
+    headers: {
+      Cookie: cookie
+    }
+  });
+
+  const $ = load(getLocationResponse.data);
+  const newViewState = $('#javax\\.faces\\.ViewState').val()?.toString();
+  if (!newViewState) {
+    throw new Error('makePostRequestToPrepareDownload: newViewState empty, aborting');
+  }
+
+  return {
+    viewState: newViewState
+  }
+}
+
+const getPdfBuffer = async (viewState: string, cookie: string) => {
+  return (await axios.post<Buffer>('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoDetalhe.jsf', 
+    new URLSearchParams({
+      gareForm: 'gareForm',
+      'gareForm:j_id81': 'Gerar GARE de Liquidação',
+      'javax.faces.ViewState': viewState,
+    }).toString(), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Cookie: cookie
+      },
+      responseType: 'arraybuffer'
+    })).data
+}
+
+export const pgeHandler: RequestHandler = async (req, res) => {
+  const { renavam } = req.body;
+
+  let { cookie, jsessionId, viewState } = await getFirstPageCookieAndJSessionIdAndViewState();
+  
+  await makePostRequestWithEmptyValues(viewState, jsessionId, cookie);
+  let formDataElementNames: string[];
+  let isMultiple = false;
+  ({ viewState, formDataElementNames, isMultiple } = await makePostRequestWithCaptchaAndValues(renavam, jsessionId, viewState, cookie));
+  
+  if (!isMultiple) {
     console.log('7. POST Download PDF (Um boleto)');
     const response = await axios.post<Buffer>('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoDetalhe.jsf', new URLSearchParams({
       gareForm: 'gareForm',
@@ -106,102 +238,22 @@ export const pgeHandler: RequestHandler = async (req, res) => {
     }).toString(), {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        Cookie: cookiesString
+        Cookie: cookie
       },
       responseType: 'arraybuffer'
     });
 
-    fs.writeFileSync('teste.pdf', response.data);
-    return res.status(200).contentType('application/pdf').send(response.data);
+    fs.writeFileSync('boleto.pdf', response.data);
+    return res.status(200).send();
   }
 
-  const formDataElements = dataTablelLinks.map(el => `gareForm:dataTable${$(el).attr('onclick')?.toString().split('gareForm:dataTable')[1].replace('\':\'', '')}`);
-
   console.log('8. POST Download PDF (Múltiplos boletos)');
-  for (const formDataElement of formDataElements) {
-    try {
-      await axios.post('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoLista.jsf', 
-      new URLSearchParams({
-        gareForm: 'gareForm',
-        'javax.faces.ViewState': viewState,
-        [formDataElement]: formDataElement
-      }).toString(), {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Cookie: cookiesString
-        },
-        maxRedirects: 0
-      })
-    } catch (err: any) {
-      if (err.response.status === 302) {
-        location = err.response.headers['location'];
-      } else {
-        throw err;
-      }
-    }
+  for (const formDataElementName of formDataElementNames) {
+    ({ viewState } = await makePostRequestToPrepareDownload(formDataElementName, viewState, cookie));
+    const pdfBuffer = await getPdfBuffer(viewState, cookie);
+    fs.writeFileSync(`${formDataElementName}.pdf`, pdfBuffer);
 
-    if (!location) {
-      return res.status(500).send();
-    }
-
-    const formElementRes = await axios.get(location.replace('http', 'https'), {
-      headers: {
-        Cookie: cookiesString
-      }
-    });
-
-    $ = load(formElementRes.data);
-    viewState = $('#javax\\.faces\\.ViewState').val()?.toString() ?? ''
-
-    const pdfBufferResponse = await axios.post<Buffer>('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoDetalhe.jsf', 
-    new URLSearchParams({
-      gareForm: 'gareForm',
-      'gareForm:j_id81': 'Gerar GARE de Liquidação',
-      'javax.faces.ViewState': viewState,
-    }).toString(), {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Cookie: cookiesString,
-        Referer: location,
-      },
-      responseType: 'arraybuffer'
-    })
-
-    fs.writeFileSync(`${formDataElement}.pdf`, pdfBufferResponse.data);
-
-    // Simula o botão voltar para navegarmos para o próximo boleto
-    try {
-      await axios.post('https://www.dividaativa.pge.sp.gov.br/sc/pages/pagamento/gareLiquidacao-pages/gareLiquidacaoDetalhe.jsf', new URLSearchParams({
-        gareForm: 'gareForm',
-        'gareForm:j_id80': 'Retornar',
-        'javax.faces.ViewState': viewState
-      }).toString(), {
-        headers: {
-          Cookie: cookiesString,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        maxRedirects: 0
-      })
-    } catch (err: any) {
-      if (err.response.status === 302) {
-        location = err.response.headers['location'];
-      } else {
-        throw err;
-      }
-    }
-
-    if (!location) {
-      return res.status(500).send();
-    }
-
-    const backButtonRes = await axios.get(location.replace('http', 'https'), {
-      headers: {
-        Cookie: cookiesString
-      }
-    });
-
-    $ = load(backButtonRes.data);
-    viewState = $('#javax\\.faces\\.ViewState').val()?.toString() ?? ''
+    ({ viewState } = await simulateBackButtonClick(viewState, cookie));
   }
 
   return res.status(200).send();
